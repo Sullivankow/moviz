5bc0bc85337072619d632b71861a171eea175aaa
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2025-01-01 17:05:12 +0100

    first commit new app project

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("ROLE_USER", "user");
-define("ROLE_ADMIN", "admin");
-define("MOVIES_IMAGES_FOLDER", "/uploads/movies/");
-define("ASSETS_IMAGES_FOLDER", "/assets/images/");
--- db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    return [
-        'db_name' => 'studi_moviz_poo',
-        'db_user' => 'root',
-        'db_password' => '',
-        'db_port' => '3306',
-        'db_host' => 'localhost'
-    ];
